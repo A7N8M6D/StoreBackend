01dbbe1196014bf3449b9f01865ab24da3aee33a
ANMD <nosha3173@gmail.com>
2024-10-17 11:03:31 +0500

    vercel.json added

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,6 +1,8 @@
+import multer from "multer"
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, '/public/temp')
+    cb(null, 'public/temp')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
@@ -8,4 +10,5 @@ const storage = multer.diskStorage({
   }
 })
 
-const upload = multer({ storage: storage })
+
+export const upload = multer({storage:storage})
