9cacbd122f0f69c01a8568c89f14a57176ef3288
ANMD <nosha3173@gmail.com>
2024-10-17 06:50:44 +0500

    vercel.json added

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,18 +2,12 @@ import multer from "multer";
 
 const storage =multer.diskStorage(
     {
-    
-        destination:function(req , res, cb){
-            console.log("Storage Level 0")
+        distanation:function(req , res, cb){
         cb(null ,"./public/temp")
-        console.log("Storage Level 1")
         },
         filename:function(req ,file ,cb)
         {
-            console.log("File Name Level 0")
-            console.log("file.originalname||file.name",file.originalname ,file.name)
             cb(null ,file.originalname)
-            console.log("File Name Level 1")
         }   
     }
 )
