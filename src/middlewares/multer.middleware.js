147d86bc7cde8a11999c1d98c7c188cb9570346e
ANMD <nosha3173@gmail.com>
2024-10-17 05:03:16 +0500

    vercel.json added

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,14 +1,19 @@
 import multer from "multer";
+import path from 'path';
+import fs from 'fs';
+const publicDir = path.join(".", 'public');
 
-const storage =multer.diskStorage(
-    {
-        destination:function(req , res, cb){
-        cb(null ,"./public")
-        },
-        filename:function(req ,file ,cb)
-        {
-            cb(null ,file.originalname)
-        }   
-    }
-)
+// Check if the public directory exists, if not, create it
+if (!fs.existsSync(publicDir)) {
+  fs.mkdirSync(publicDir);
+}
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, publicDir); // Save files to the public directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); // Save file with its original name
+  },
+});
 export const upload = multer({storage,})
