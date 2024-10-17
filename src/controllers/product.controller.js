8f70b055b1fb9e9e8322d3ecbdee80f3bb6a660b
ANMD <nosha3173@gmail.com>
2024-10-17 13:41:22 +0500

    vercel.json added

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -13,12 +13,12 @@ const addProduct = async (req, res) => {
   try {
     console.log(`Product Add Controller`);
     const User = req.user.id;
-    console.log(`User ${User}`);
     // if (!req.files || req.files.length === 0) {
-    //   return res.status(400).json({ error: "No files uploaded" });
-    // }
-    const images = req.files.images.map((file) => file.path);
-   
+      //   return res.status(400).json({ error: "No files uploaded" });
+      // }
+      const images = req.files.images.map((file) => file.path);
+      
+      console.log(`User ${User}`);
     console.log(`Images ${images[0]}`);
     let { brand, quantity, price, category ,name} = req.body;
     quantity = parseInt(quantity, 10);
